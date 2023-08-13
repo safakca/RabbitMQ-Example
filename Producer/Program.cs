97110d4f68b6e1b1e85a40c6d7ac9efa8d36f76f
safakca <safakkca@hotmail.com>
2023-08-13 23:12:59 +0300

    …

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -13,6 +13,7 @@ Customer customer = new()
     Message="Siparişiniz Yolda"
 };
 
+
 ConnectionFactory connectionFactory = new()
 {
     Uri = new Uri("amqps://geilrjnf:CFHBVh0ApbH-4IfPGkSEB7-hUFVg1wQK@sparrow.rmq.cloudamqp.com/geilrjnf")
